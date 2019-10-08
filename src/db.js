afba82a5dac42817f70eff07c8504a965e1f1322
Xefise <Xefise@gmail.com>
2019-10-08 22:16:46 +0300

    Add function endConnect and add comments:b

--- src/db.js
+++ src/db.js
@@ -1,23 +1,33 @@
 var mysql = require('mysql');
 
-function connect() {
-    let con = mysql.createConnection({
+var DBpassword = "add input";//TODO: Add input, functions and queries
+
+function connect() { // Don't hit me pls
+    let connection = mysql.createConnection({
         host: "localhost",
         user: "admin",
-        password: "password" //TODO: Add input, functions and queries
+        password: DBpassword
     });
 
-    con.connect(function (err) { //connect 
+    connection.connect(function (err) { //connect 
+        if (err) throw err; // Throw error in case error.. WOW this is genius! Nobody could not think about it!
+        console.log("Connected to the database! YEAH! WE DID IT!");
+        return connection;
+    });
+}
+
+function endConnect(connection) { // stop... IT'S END CONNECTION?! FOR WHAT?!
+    connection.end(function (err) {
         if (err) throw err;
-        console.log("Connected!");
-        return con;
+        console.log("A connection is closed:)");
     });
 }
 
 function select(table) {
-    con = connect();
-    con.query(`SELECT * FROM ${table}`, function (err, result, fields) {
+    let connection = connect();
+    connection.query(`SELECT * FROM ${table}`, function (err, result, fields) {
         if (err) throw err;
+        endConnect(connection);
         return result, fields;
     });
 }
