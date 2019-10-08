b58dc182e90be7481d9d4cbf87cf7825128af733
Xefise <Xefise@gmail.com>
2019-10-08 22:04:16 +0300

    Add functions: select and connect xD

--- src/db.js
+++ src/db.js
@@ -1,12 +1,23 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "admin",
-    password: "password" //TODO: Add input, functions and queries
-});
+function connect() {
+    let con = mysql.createConnection({
+        host: "localhost",
+        user: "admin",
+        password: "password" //TODO: Add input, functions and queries
+    });
 
-con.connect(function (err) { //connect 
-    if (err) throw err;
-    console.log("Connected!");
-});
+    con.connect(function (err) { //connect 
+        if (err) throw err;
+        console.log("Connected!");
+        return con;
+    });
+}
+
+function select(table) {
+    con = connect();
+    con.query(`SELECT * FROM ${table}`, function (err, result, fields) {
+        if (err) throw err;
+        return result, fields;
+    });
+}
