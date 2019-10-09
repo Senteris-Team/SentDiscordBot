aea65f278474d27c9cc51775726fac1c632e6a2d
Xefise <Xefise@gmail.com>
2019-10-09 20:51:37 +0300

    Now we can send arrays of values to insert

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var DBpassword = "add input";//TODO: Add input, functions and queries
 
-function connect() { // Don't hit me pls
+function connect() {
     let connection = mysql.createConnection({
         host: "localhost",
         user: "admin",
@@ -35,8 +35,22 @@ function select(table) {
 }
 
 function insert(table, column, value) {
+    let columns;
+    if( Object.prototype.toString.call( someVar ) === '[object Array]' ) { // if column == array => columns = "column1, column2..."
+        for (i = 0; i != column.length; i++) {
+            columns += column[i] + ", ";
+        } columns += column [-0]; // columns = "... column5, column6 (w/o ',')"
+    } else { columns = column; } // else columns == column
+
+    let values;
+    if( Object.prototype.toString.call( someVar ) === '[object Array]' ) { //Same
+        for (i = 0; i != value.length; i++) {
+            values += value[i] + ", ";
+        } values += value [-0];
+    } else { values = value; }
+
     let connection = connect();
-    connection.query(`INSERT INTO ${table} (${column}) values (${value})`, function (err, result, fields) {
+    connection.query(`INSERT INTO ${table} (${columns}) values (${values})`, function (err, result, fields) {
         if (err) throw err;
         endConnect(connection);
     });
