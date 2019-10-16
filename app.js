95c06e1e8854b31b770b460e9ab7ddc719e4052c
Neisvestney <neisvestney113@gmail.com>
2019-10-16 21:32:54 +0300

    update outut

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ client.on("message", message => {
       let mutetime = command[2];
       if (!mutetime) return message.reply("You didn't specify a time!");
       message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
-      log(`mute ${tomute.id}`, "Guild " + message.guild.id, message.member.tag);
+      log(`mute ${tomute.id}`, "Guild " + message.guild.tag, message.member.tag);
       setTimeout(function() {
         tomute.removeRole(muterole.id);
         message.channel.send(`<@${tomute.id}> has been unmuted!`);
