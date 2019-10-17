63ae656cd6fb0c994b033175dd9d33c004051313
Neisvestney <neisvestney113@gmail.com>
2019-10-17 19:47:34 +0300

    command -> args

--- commands/settings/getsettings.js
+++ commands/settings/getsettings.js
@@ -3,11 +3,11 @@ const { log } = require("../../functions.js");
 
 exports.run = (client, message, args) => {
     if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**Error:** You do not have the need permission!");
-    if (command.length < 3) return message.reply("**Error:** Not enough arguments.");
-    if (command[1] == "guild_id") return message.reply("**Error:** You cannot change guild id!")
+    if (args.length < 3) return message.reply("**Error:** Not enough arguments.");
+    if (args[1] == "guild_id") return message.reply("**Error:** You cannot change guild id!")
 
-    var valueToUpdate = command.slice(2, command.length).join(" ");
-    db.update("settings", command[1], valueToUpdate, "`guild_id`", message.guild.id, message)
-    message.reply(`Setting ${command[1]} updated to '${valueToUpdate}'`)
+    var valueToUpdate = args.slice(2, args.length).join(" ");
+    db.update("settings", args[1], valueToUpdate, "`guild_id`", message.guild.id, message)
+    message.reply(`Setting ${args[1]} updated to '${valueToUpdate}'`)
     log(`update settings`, "Guild " + message.guild, message.member.tag);
 }
