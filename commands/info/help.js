392dc68d606f0e712eef6058504fa87ae376a6e3
Neisvestney <neisvestney113@gmail.com>
2019-10-17 19:17:23 +0300

    aga sasno

--- commands/info/help.js
+++ commands/info/help.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
         `${client.config.prefix}mute *user* *time**Unit*\n` +
         `// *Unit* (time unit) can be s, m, h, d. for example ${client.config.prefix}mute TSDoge 666d\n` +
         `${client.config.prefix}unmute *user*\n` +
-        `${client.config.client.config.prefix}getsettings\n` +
+        `${client.config.prefix}getsettings\n` +
         `${client.config.prefix}setsettings *setting_name* *setting_var* //For developers\n` +
         `${client.config.prefix}homework\n` +
         "```"
