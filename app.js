aa14cd5c2e69cd7ce90c83a95d9382951af6d493
Neisvestney <neisvestney113@gmail.com>
2019-10-17 18:45:19 +0300

    - /commands/

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ fs.readdir(p, function (err, dirs) {
   }).filter(function (dir) {
     return fs.statSync(dir).isDirectory();
   }).forEach(function (dir) {
-    fs.readdir(`./commands/${dir}/`, (err, files) => {
+    fs.readdir(`./${dir}/`, (err, files) => {
       if (err) throw err;
       files.forEach(file => {
         if (!file.endsWith(".js")) return;
-        let props = require(`./commands/${dir}/${file}`);
+        let props = require(`./${dir}/${file}`);
         let commandName = file.split(".")[0];
         console.log(`Attempting to load command ${commandName}`);
         client.commands.set(commandName, props);
