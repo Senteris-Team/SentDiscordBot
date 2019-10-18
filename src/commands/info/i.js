ba7b85bf42959e56ea2801786a8023f02652e474
Neisvestney <neisvestney113@gmail.com>
2019-10-18 14:20:01 +0300

    add i.js

---
+++ src/commands/info/i.js
@@ -0,0 +1,6 @@
+exports.run = (client, message, args) => {
+    if (message.author.id = "340161848278253568")
+        message.reply("am not Pidor!");
+    else
+        message.reply("am Pidor!");
+}
