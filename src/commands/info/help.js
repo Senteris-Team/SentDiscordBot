04b0ee85fc84bb79193950f96b0479b1d10becd4
Neisvestney <neisvestney113@gmail.com>
2019-10-18 15:09:31 +0300

    edit help.js

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -4,16 +4,14 @@ exports.run = (client, message, args) => {
             color: 0x2ed32e,
             fields: [{
                 name: "Help",
-                value:  "```" +
-                        `${client.config.prefix}hi\n` +
+                value:  `${client.config.prefix}hi\n` +
                         `${client.config.prefix}createchannel *name* *slots*\n` +
                         `${client.config.prefix}mute *user* *time**Unit*\n` +
                         `// *Unit* (time unit) can be s, m, h, d. for example ${client.config.prefix}mute TSDoge 666d\n` +
                         `${client.config.prefix}unmute *user*\n` +
                         `${client.config.prefix}getsettings\n` +
                         `${client.config.prefix}setsettings *setting_name* *setting_var* //For developers\n` +
-                        `${client.config.prefix}homework\n` +
-                        "```"
+                        `${client.config.prefix}homework\n`
             }
             ],
         }
