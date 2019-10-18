0dce4daf307642e91338daf135926fb0cafe1068
Neisvestney <neisvestney113@gmail.com>
2019-10-18 14:04:49 +0300

    fix patch

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ fs.readdir(p, function (err, dirs) {
   }).filter(function (dir) {
     return fs.statSync(dir).isDirectory();
   }).forEach(function (dir) {
-    fs.readdir(`./src/${dir}/`, (err, files) => {
+    fs.readdir(`./${dir}/`, (err, files) => {
       if (err) throw err;
       files.forEach(file => {
         if (!file.endsWith(".js")) return;
