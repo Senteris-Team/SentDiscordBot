3b3f588721c413fc821d62182166d81263c79023
Xefise <Xefise@gmail.com>
2019-10-19 21:29:09 +0300

    Update help

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -1,5 +1,16 @@
 exports.run = (client, message, args) => {
-  message.channel.send({
+  if (!message.member.hasPermission("ADMINISTRATOR")) message.channel.send({
+    embed: {
+      color: 0x2ed32e,
+      fields: [{
+        name: "Help",
+        value:  `${client.config.prefix}hi\n` +
+          `${client.config.prefix}createchannel *name* *slots*\n` +
+          `${client.config.prefix}ping`
+      }],
+    }
+  });
+  else message.channel.send({ // for administrators
     embed: {
       color: 0x2ed32e,
       fields: [{
@@ -7,11 +18,11 @@ exports.run = (client, message, args) => {
         value:  `${client.config.prefix}hi\n` +
           `${client.config.prefix}createchannel *name* *slots*\n` +
           `${client.config.prefix}mute *user* *time**Unit*\n` +
-          `// *Unit* (time unit) can be s, m, h, d. for example ${client.config.prefix}mute TSDoge 666d\n` +
+          `// *Unit* (time unit) can be s, m, h, d. for example ${client.config.prefix}mute @N0Name#4213 666h\n` +
           `${client.config.prefix}unmute *user*\n` +
           `${client.config.prefix}getsettings\n` +
           `${client.config.prefix}setsettings *setting_name* *setting_var* //For developers\n` +
-          `${client.config.prefix}homework\n`
+          `${client.config.prefix}ping`
       }],
     }
   });
