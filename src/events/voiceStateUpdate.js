e0bdf2ff3cedb52c7d42e93638470b7138372187
Xefise <Xefise@gmail.com>
2019-11-10 19:36:39 +0300

    Add check for category name

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -5,11 +5,18 @@ module.exports = (client, oldMember, newMember) => {
     db.get_giuld_settings(oldMember.guild, resolve);
   }).then(function (settings) {
     if (oldMember.voiceChannel) {
-      if (oldMember.voiceChannel.members.size == 0) {
-        var noDelete = settings.white_channel_list;
-        if (!noDelete.includes(oldMember.voiceChannel.name)) 
-          oldMember.voiceChannel.delete();
-      }
+      if (oldMember.voiceChannel.members.size != 0) return;
+
+      new Promise(function (resolve) {
+        db.select("settings", "guild_id", guild.id, resolve); // Get settings
+      }).then(function (settings) {
+        if(settings.voice_channels_category.name != oldMember.voiceChannel.parent.name) return;
+      });
+
+      var noDelete = settings.white_channel_list;
+      if (noDelete.includes(oldMember.voiceChannel.name)) return;
+
+      oldMember.voiceChannel.delete();
     }
   });
 }
