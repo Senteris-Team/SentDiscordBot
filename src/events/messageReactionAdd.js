23e06a223fc5f4fd8c203dc08ffd18cedcacf511
Xefise <Xefise@gmail.com>
2019-11-22 16:17:16 +0300

    Debug and change message

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -1,16 +1,18 @@
 const { log } = require("../functions.js");
 
 module.exports = (client, messageReaction, user) => {
-  //console.log("Robit"); // for debug
+  console.log("messageReactionAdd"); // for debug
   var message = messageReaction.message;
-  //console.log("Channel.name: "+ messageReaction.message.channel.name); // for debug
 
-  if(message.id == "646435588827774997" && message.channel.name === "welcome-to-the-club-buddy"){   
+  if(message.id == "646435588827774997" && message.channel.name === "welcome-to-the-club-buddy"){
+    console.log("True"); // for debug
     let role = message.guild.roles.find(role => role.name.toLowerCase() === messageReaction.emoji.name.toLowerCase());
     log("required role " + role.name);
     if (!role) return; // We can add log `${messageReaction.emoji.name} role doesn't exist` to logs channel
+    console.log("Role exist"); // for debug
     let member = message.guild.members.find(member => member.id === user.id);
     if (!member) return; // We can add log too
+    console.log("Member exist"); // for debug
 
     try { member.roles.add(role); }
     catch(err) { log(err) }
--- src/events/messageReactionRemove.js
+++ src/events/messageReactionRemove.js
@@ -1,14 +1,18 @@
 const { log } = require("../functions.js");
 
 module.exports = (client, messageReaction, user) => {
+  console.log("messageReactionRemove"); // for debug
   var message = messageReaction.message;
 
   if(message.id == "646435588827774997" && message.channel.name === "welcome-to-the-club-buddy"){
+    console.log("True"); // for debug
     let role = message.guild.roles.find(role => role.name.toLowerCase() === messageReaction.emoji.name.toLowerCase());
     log("required delete role " + role.name);
     if (!role) return; // We can add log `${messageReaction.emoji.name} role doesn't exist` to logs channel
+    console.log("Role exist"); // for debug
     let member = message.guild.members.find(member => member.id === user.id);
     if (!member) return; // We can add log too
+    console.log("Member exist"); // for debug
 
     try { member.roles.remove(role); }
     catch(err) { log(err) } //for debug
