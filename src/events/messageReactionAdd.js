ed737cbd1507ced3410683522ae09de1bf30ade1
Neisvestney <neisvestney113@gmail.com>
2019-11-22 14:50:40 +0300

    update reaction

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -1,12 +1,13 @@
 const { log } = require("../functions.js");
 
 module.exports = (client, messageReaction, user) => {
-  console.log("Robit"); // for debug
+  //console.log("Robit"); // for debug
   var message = messageReaction.message;
-  console.log("Channel.name: "+ messageReaction.message.channel.name); // for debug
+  //console.log("Channel.name: "+ messageReaction.message.channel.name); // for debug
 
-  if(message.id == "647151954438651914" && message.channel.name === "новенькие"){
+  if(message.id == "646435588827774997" && message.channel.name === "welcome-to-the-club-buddy"){   
     let role = message.guild.roles.find(role => role.name.toLowerCase() === messageReaction.emoji.name.toLowerCase());
+    log("required role " + role.name);
     if (!role) return; // We can add log `${messageReaction.emoji.name} role doesn't exist` to logs channel
     let member = message.guild.members.find(member => member.id === user.id);
     if (!member) return; // We can add log too
