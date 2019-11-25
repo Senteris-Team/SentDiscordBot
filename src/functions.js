e6f4c91ec1ee5b410d159c0495a30865463ccd2a
Xefise <Xefise@gmail.com>
2019-11-25 20:26:10 +0300

    Add sending message to channel logs

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 
-function log(message, where = "BOT", who = "") {
+function log(message, guild, where = "BOT", who = "") {
+
+  logs_channel_id = 647523442589171714;
+
   var now = new Date();
   var options = {
     year: 'numeric',
@@ -11,8 +14,12 @@ function log(message, where = "BOT", who = "") {
     second: 'numeric'
   };
   let date = now.toLocaleString("ru", options);
-  let log_str = `[${date}] (${where}) ${who} ${message}`
-  console.log(log_str)
+
+  let logs_channel = guild.channels.find(channel => channel.id === logs_channel_id);
+
+  console.log(`[${date}] (${where})${who} ${message}`);
+  logs_channel.send(`[${date}] ${who} ${message}`)
+    .catch(console.error); // then change
 
   fs.appendFile("log.txt", log_str, function (error) {
     if (error) throw console.error(error);
