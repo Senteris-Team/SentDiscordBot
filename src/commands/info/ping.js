2864aa0ce17abe7c0afd61aa893e7480bde11611
Xefise <Xefise@gmail.com>
2019-11-28 23:49:20 +0300

    Update ping

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,11 +1,8 @@
 exports.run = (client, message, args) => {
-  message.channel.send({
-    embed: {
-      color: 0x2ed32e,
-      fields: [{
-        name: "Pong",
-        value: "My Ping: " + Math.round(client.ping) + ' ms'
-      }],
-    }
+  let pingTimestamp = message.createdTimestamp;
+  message.channel.send('Ping!')
+  .then((reply) => {
+    let pongTimestamp = reply.createdTimestamp;
+    reply.edit(`Pong! Ping: \`${pongTimestamp-pingTimestamp}ms\``);
   });
 }
