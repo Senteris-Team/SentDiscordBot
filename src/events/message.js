31a2761bfca684a48207c36e288c3e1d21fe720f
Xefise <Xefise@gmail.com>
2019-12-01 23:23:18 +0300

    fix message

--- src/events/message.js
+++ src/events/message.js
@@ -1,23 +1,22 @@
 const db = require("../db.js");
 
 module.exports = (client, message) => {
-  var prefix;
   new Promise(function (resolve) {
     db.getGuild(message.guild, resolve);
   }).then(function (guildDB) {
-    prefix = guildDB.prefix;
+    const prefix = guildDB.prefix;
+
+    if (message.author.bot) return;
+    if (!message.guild) return;
+
+    if (message.content.indexOf(prefix) !== 0) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    const cmd = client.commands.get(command);
+
+    if (!cmd) return message.reply("**ERROR**: No this command!");
+    cmd.run(client, message, args);
   });
-
-  if (message.author.bot) return;
-  if (!message.guild) return;
-
-  if (message.content.indexOf(prefix) !== 0) return;
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-  const cmd = client.commands.get(command);
-
-  if (!cmd) return message.reply("**ERROR**: No this command!");
-  cmd.run(client, message, args);
 };
