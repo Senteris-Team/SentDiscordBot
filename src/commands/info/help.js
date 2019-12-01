05757eb93165dfc82f3d72b25aa449e33e472dc7
Xefise <Xefise@gmail.com>
2019-12-01 23:19:34 +0300

    fix help and voiceStateUpdate

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -1,38 +1,37 @@
 const db = require("../../db.js");
 
 exports.run = (client, message, args) => {
-  
-  var prefix;
+
   new Promise(function (resolve) {
     db.getGuild(message.guild, resolve);
   }).then(function (guildDB) {
-    prefix = guildDB.prefix;
-  });
+    const prefix = guildDB.prefix;
 
-  if (!message.member.hasPermission("ADMINISTRATOR")) message.channel.send({
-    embed: {
-      color: 0x2ed32e,
-      fields: [{
-        name: "Help",
-        value: `${prefix}hi\n` +
-          `${prefix}createchannel *name* *slots*\n` +
-          `${prefix}ping`
-      }],
-    }
-  });
-  else message.channel.send({ // for administrators
-    embed: {
-      color: 0x2ed32e,
-      fields: [{
-        name: "Help",
-        value: `${prefix}hi\n` +
-          `${prefix}createchannel *name* *slots*\n` +
-          //`${prefix}mute *user* *time**Unit*\n` +
-          //`// *Unit* (time unit) can be s, m, h, d. for example ${client.config.prefix}mute @N0Name#4213 666h\n` +
-          //`${prefix}unmute *user*\n` +
-          `${prefix}getsettings\n` +
-          `${prefix}ping`
-      }],
-    }
-  });
+    if (!message.member.hasPermission("ADMINISTRATOR")) message.channel.send({
+      embed: {
+        color: 0x2ed32e,
+        fields: [{
+          name: "Help",
+          value: `${prefix}hi\n` +
+            `${prefix}createchannel *name* *slots*\n` +
+            `${prefix}ping`
+        }],
+      }
+    });
+    else message.channel.send({ // for administrators
+      embed: {
+        color: 0x2ed32e,
+        fields: [{
+          name: "Help",
+          value: `${prefix}hi\n` +
+            `${prefix}createchannel *name* *slots*\n` +
+            //`${prefix}mute *user* *time**Unit*\n` +
+            //`// *Unit* (time unit) can be s, m, h, d. for example ${client.config.prefix}mute @N0Name#4213 666h\n` +
+            //`${prefix}unmute *user*\n` +
+            `${prefix}getsettings\n` +
+            `${prefix}ping`
+        }],
+      }
+    });
+});
 }
--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -2,7 +2,7 @@ const db = require("../db.js");
 
 module.exports = (client, oldMember, newMember) => {
   new Promise(function (resolve) {
-    db.select("guilds", "guild_id", oldMember.guild.id, resolve);
+    db.select("guilds", "guildID", oldMember.guild.id, resolve);
   }).then(function (guildDB) {
     if (oldMember.voiceChannel) {
       if (oldMember.voiceChannel.members.size != 0) return;
