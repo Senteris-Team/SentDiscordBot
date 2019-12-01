f29265df73b78da3f3d43e0b26f985e195634dc9
Xefise <Xefise@gmail.com>
2019-12-01 23:24:23 +0300

    fix event VSU

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -2,11 +2,11 @@ const db = require("../db.js");
 
 module.exports = (client, oldMember, newMember) => {
   new Promise(function (resolve) {
-    db.select("guilds", "guildID", oldMember.guild.id, resolve);
+    db.getGuild(oldMember.guild, resolve);
   }).then(function (guildDB) {
     if (oldMember.voiceChannel) {
       if (oldMember.voiceChannel.members.size != 0) return;
-      if(guildDB.voiceChannelsCategory != oldMember.voiceChannel.parent.name) return;
+      if (guildDB.voiceChannelsCategory != oldMember.voiceChannel.parentID) return;
 
       const noDelete = guildDB.whiteChannels;
       if (noDelete.includes(oldMember.voiceChannel.name)) return;
