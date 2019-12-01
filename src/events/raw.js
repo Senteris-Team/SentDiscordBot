aa66429b0404e15c50febd174b76e04f2faf19d9
Xefise <Xefise@gmail.com>
2019-12-01 23:37:25 +0300

    fixed

--- src/events/raw.js
+++ src/events/raw.js
@@ -2,24 +2,17 @@ module.exports = (client, packet) => { // Copypasted by Neis:)
   // We don't want this to run on unrelated packets
   if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
   // Grab the channel to check the message from
-  console.log("1");
   const channel = client.channels.get(packet.d.channel_id);
   // There's no need to emit if the message is cached, because the event will fire anyway for that
-  console.log("2");
   if (channel.messages.has(packet.d.message_id)) return;
-  console.log("3");
   // Since we have confirmed the message is not cached, let's fetch it
   channel.fetchMessage(packet.d.message_id).then(message => {
-    console.log("4");
       // Emojis can have identifiers of name:id format, so we have to account for that case as well
       const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
-      console.log("5");
       // This gives us the reaction we need to emit the event properly, in top of the message object
       const reaction = message.reactions.get(emoji);
-      console.log("6");
       // Adds the currently reacting user to the reaction's users collection.
       if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
-      console.log("7");
       // Check which type of event it is before emitting
       if (packet.t === 'MESSAGE_REACTION_ADD') {
           client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
