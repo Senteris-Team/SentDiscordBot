e4adcc8bcaf64203a2af69ec88152bd9eb7830fd
Xefise <Xefise@gmail.com>
2019-12-13 20:58:34 +0300

    Add getting prefix

--- src/commands/guides/guides.js
+++ src/commands/guides/guides.js
@@ -1,14 +1,19 @@
 exports.run = (client, message, args) => {
-  message.channel.send({
-    embed: {
-      color: 0x2ed32e,
-      fields: [{
-        name: "Guides",
-        value: `${p}BaseGuide\n` +
-          `${p}VoiceChannelsGuide\n` +
-          `${p}GameRolesGuide\n` +
-          "Гайды пока что только на русском языке:("
-      }],
-    }
+  new Promise(function (resolve) {
+    db.getGuild(message.guild, resolve);
+  }).then(function (guildDB) {
+    const p = guildDB.prefix;
+    message.channel.send({
+      embed: {
+        color: 0x2ed32e,
+        fields: [{
+          name: "Guides",
+          value: `${p}BaseGuide\n` +
+            `${p}VoiceChannelsGuide\n` +
+            `${p}GameRolesGuide\n` +
+            "Гайды пока что только на русском языке:("
+        }],
+      }
+    });
   });
 }
