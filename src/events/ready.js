9b653e00761811de923d7c683743928bfcbaa575
Xefise <Xefise@gmail.com>
2020-07-23 10:20:29 +0300

    sdfsdf

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,16 +4,5 @@ module.exports = (client) => {
   log(`logged in as ${client.user.tag}!`, undefined , "BOT", "Client");
 
   client.user.setActivity("Watching you:b| !-help v0.3", { type: 1 });
-  client.user.setPresence({/*
-    status: "idle",
-    activity: {
-      type: "CUSTOM_STATUS",
-      name: "Testing"
-    }*/
-    status: "idle",
-    activity: {
-      type: "CUSTOM_STATUS",
-      name: "Watching you:b| !-help v0.3"
-    }
-  });
+
 }
